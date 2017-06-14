bc59332023147a180b5d742328316ce97feb32b6
Sachin Arote <sachinar@cybage.com>
2017-06-14 14:35:05 +0530

    Create main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello GCP CD!")
+	io.WriteString(w, "Hello Sachin Done POC on Spinnaker!")
 }
 
 func main() {
