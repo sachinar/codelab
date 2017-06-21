a389faf35e11f80e3295c7bdac1326c0228e2a79
Sachin Arote <sachinar@cybage.com>
2017-06-21 18:30:02 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello Sachin Done POC on Spinnaker! Wow its running")
+	io.WriteString(w, "Hello Sachin Done POC on Spinnaker! Wow its running!!!")
 }
 
 func main() {
